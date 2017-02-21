83dafc5f32a7e9d8e58cc4542628f542b171227f
Hans Kilian <hkkilian@gmail.com>
2017-02-21 10:39:11 +0100

    Added databinding to home page

--- Home.jsx
+++ Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import ActionSearch from 'material-ui/svg-icons/action/search';
+import {fullWhite} from 'material-ui/styles/colors';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const style = {
+  margin: 12,
+};
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { invId: "" };
+  }
+
+  handleInvIdChange(event) {
+    this.setState({ invId: event.target.value });
+  }
+
   render() {
-    return (<h1>Home</h1>);
+    return (<Paper style={{marginTop: '280px'}}>
+              <TextField hintText="Kundenummer" onChange={this.handleInvIdChange.bind(this)} style={{margin: 12, width: '725px'}} />
+              <FlatButton backgroundColor="#a4c639" hoverColor="#8AA62F" icon={<ActionSearch color={fullWhite} />} style={{margin: 12}} />
+            </Paper>);
   }
 };
 
